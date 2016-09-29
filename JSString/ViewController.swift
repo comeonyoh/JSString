711e40ef83fc6b987b85e23cb46d2e1a7ea0e8d1
kimJungSu <comeonyoh@tigrison.com>
2016-09-29 16:26:11 +0900

    unused method removed

--- JSString/ViewController.swift
+++ JSString/ViewController.swift
@@ -3,23 +3,10 @@
 //  JSString
 //
 //  Created by KimJungSu on 2016. 9. 29..
-//  Copyright © 2016년 ODOV. All rights reserved.
+//  Copyright © 2016 ODOV. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+class ViewController: UIViewController {}
 
